977c7c448d197e609146ff47879f2a875c92657e
mackowacko <mackowacko186@gmail.com>
2014-04-25 16:11:59 -0700

    pythonroom update

--- draw.py
+++ draw.py
@@ -1,1 +1,21 @@
 # author: mackowacko
+
+import turtle
+
+length = 75
+angle = 60
+
+Jose = turtle.turtle
+
+Jose.forward(length)
+Jose.left(angle)
+Jose.forward(length)
+Jose.left(angle)
+Jose.forward(length)
+Jose.left(angle)
+Jose.forward(length)
+Jose.left(angle)
+Jose.forward(length)
+Jose.left(angle)
+Jose.forward(length)
+Jose.left(angle)
