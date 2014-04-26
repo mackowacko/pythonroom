e8e929f795dd7bda46a172353e92111b95579123
mackowacko <mackowacko186@gmail.com>
2014-04-25 17:42:41 -0700

    pythonroom update

--- draw.py
+++ draw.py
@@ -1,13 +1,15 @@
 # author: mackowacko 
 
-colors = ["red", "yellow", "blue", "magenta"]
+#colors = ["red", "yellow", "blue", "magenta"]
+length = 30
 angle = 90
-numbers = range (5, 200, 2)
+numbers = range (5, 200)
+colors = ["red", "orange", "magenta", "blue"]
 
-Jose = turtle.Turtle()
+	Jose = turtle.Turtle()
 
-for color in colors:
-	for number in numbers:
-		Jose.forward(number)
+for number in numbers:
+	for color in colors: 
+		Jose.forward(numbers)
 		Jose.left(angle)
 	
